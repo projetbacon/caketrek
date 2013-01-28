7ddcfa920537f5669d82089cc6210b0c6f9e4738
Flochataignier <florent.chataignier@gmail.com>
2013-01-28 11:01:05 +0100

    bake zone

---
+++ app/Model/Zone.php
@@ -0,0 +1,41 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Zone Model
+ *
+ * @property Journey $Journey
+ */
+class Zone extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'id';
+
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'Journey' => array(
+			'className' => 'Journey',
+			'foreignKey' => 'zone_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+}
