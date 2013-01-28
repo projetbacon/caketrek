a129c7fb6f4e0046d32d9ae59bbf84c1b4617670
cecilesab <cecilesablayrolles@mac.com>
2013-01-28 11:00:33 +0100

    add Bake Journeys

---
+++ app/Model/Journey.php
@@ -0,0 +1,58 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Journey Model
+ *
+ * @property Tourist $Tourist
+ * @property Guide $Guide
+ * @property Track $Track
+ * @property Zone $Zone
+ */
+class Journey extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'Tourist' => array(
+			'className' => 'Tourist',
+			'foreignKey' => 'tourist_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Guide' => array(
+			'className' => 'Guide',
+			'foreignKey' => 'guide_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Track' => array(
+			'className' => 'Track',
+			'foreignKey' => 'track_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Zone' => array(
+			'className' => 'Zone',
+			'foreignKey' => 'zone_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
---
+++ app/Test/Case/Model/JourneyTest.php
@@ -0,0 +1,47 @@
+<?php
+App::uses('Journey', 'Model');
+
+/**
+ * Journey Test Case
+ *
+ */
+class JourneyTest extends CakeTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.journey',
+		'app.tourist',
+		'app.user',
+		'app.badge',
+		'app.badges_user',
+		'app.guide',
+		'app.track',
+		'app.zone'
+	);
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		$this->Journey = ClassRegistry::init('Journey');
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		unset($this->Journey);
+
+		parent::tearDown();
+	}
+
+}
