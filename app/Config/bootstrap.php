4d7e88202554bdb27a8025b0eb3ee8f4449e981c
cecilesab <cecilesablayrolles@mac.com>
2013-01-28 10:04:06 +0100

    getting loose of the Media Plugin

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -143,7 +143,7 @@ Cache::config('default', array('engine' => 'File'));
  *
  */
 
-CakePlugin::load('Media');
+//CakePlugin::load('Media');
 
 /**
  * You can attach event listeners to the request lifecyle as Dispatcher Filter . By Default CakePHP bundles two filters:
