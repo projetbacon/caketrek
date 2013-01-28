a56d75817a79f11c91ac096c20e22f4b4c898c8f
Clemeeent <clement.lebau@gmail.com>
2013-01-28 11:00:00 +0100

    Bake Track

---
+++ app/Test/Fixture/TrackFixture.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * TrackFixture
+ *
+ */
+class TrackFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'primary'),
+		'indexes' => array(
+			
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1
+		),
+	);
+
+}
